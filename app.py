9f6494597a3f89385820e869bba34e599cfccfaf
Yogesh <93325653+Yoge-sh@users.noreply.github.com>
2024-02-21 13:24:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import google.generativeai as genai
 
 from youtube_transcript_api import YouTubeTranscriptApi
 
-# genai.configure(api_key=GOOGLE_API_KEY)
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY")) # replace with your own key
 
 
 def extract_transcript_details(youtube_video_url):
@@ -31,14 +30,27 @@ def generate_gemini_content(transcript_text,prompt):
 
 st.title('Youtube Video ➡️ Quick Notes')
 
-youtube_link = st.text_input('Enter the youtube vide link :')
+youtube_link = st.text_input('Enter the youtube video link (video URL without timestamp):')
 subject = st.text_input('Enter the topic explained in the video :')
 
-prompt = f"""You are Youtube video summarizer for the given transcript of the youtube video on {subject} You will be taking the
- transcript text and Summarize the key concepts and insights from a this transcript on  {subject}  Provide concise and comprehensive 
- notes, covering major points, definitions, and any notable examples or applications mentioned in the transcript text. 
+prompt = f"""You are Youtube video summarizer for the given transcript of the youtube video on {subject}.
+You will be taking the transcript text and Summarize the key concepts and insights from a this transcript on  {subject}  Provide concise and 
+comprehensive notes, covering major points, definitions, and any notable examples or applications mentioned in the transcript text. 
 Your goal is to create an informative and organized set of notes that captures the essence of the video for someone seeking a 
-brief understanding of the subject matter with help of main points discussed under the topic  {subject} """
+brief understanding of the subject matter with help of main points discussed under the topic  {subject}. 
+Keep the notes descriptive and major concepts should be in bold and followed by the explanation of it in bullet points like following:
+
+Subject: Neural Network
+Perceptron:
+    Basic building block.
+    Discovery of 60s and 70s.
+Multilevel Perceptron:
+    Multiple percenptron are next big discovery and opens up door for research again.
+    Basic block of deep neural networks 
+    
+and so on....
+"""
+
 
 if youtube_link:
     video_id = youtube_link.split("=")[1]
